952c10de0daaf0fb6efc40b59cc57f561d770bd1
dirtmelon <0xffdirtmelon@gmail.com>
2022-01-17 09:38:42 +0800

    restaurant

---
+++ restaurant/src/lib.rs
@@ -0,0 +1,70 @@
+use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    //Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    //Relative path
+    front_of_house::hosting::add_to_waitlist();
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // field `seasonal_fruit` of struct `Breakfast` is private
+    // meal.seasonal_fruit = String::from("blueberries");
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+
+    hosting::add_to_waitlist();
+}
+
+fn serve_order() {
+    
+}
+
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
+    }
+}
+
+mod back_of_house {
+    // 枚举只需要在 enum 前添加 pub 关键字
+    pub enum Appetize {
+        Soup,
+        Salad,
+    }
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    fn fix_incorrect_order() {
+        cook_order();
+        super::serve_order();
+    }
+
+    fn cook_order() {}
+}
