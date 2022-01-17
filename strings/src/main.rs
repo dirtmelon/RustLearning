2281be3b532f5a356c21ef1c1e4f35f4bfb4d417
dirtmelon <0xffdirtmelon@gmail.com>
2022-01-17 12:01:03 +0800

    strings

---
+++ strings/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let mut s = String::from("lo");
+    s.push('l');
+
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2;
+    println!("{}", s3);
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = format!("{}-{}-{}", s1, s2, s3);
+    println!("{}", s);
+}
