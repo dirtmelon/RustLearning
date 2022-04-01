98036e2fab251c8139862605f1724ba283c433c7
dirtmelon <0xffdirtmelon@gmail.com>
2022-04-01 22:44:36 +0800

    adder

---
+++ adder/src/lib.rs
@@ -0,0 +1,63 @@
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn another() {
+        panic!("Make this test fail");
+    }
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        }
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        }
+
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    #[should_panic]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+pub struct Guess {
+    value: u32,
+}
+
+impl Guess {
+    pub fn new(value: u32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got P{}", value);
+        }
+        Guess {
+            value
+        }
+    }
+}
+
